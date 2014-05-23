4dbc6a8c0be80f7fc1dd4035e0d794fc9a9bab1a
ThrostGunnulf <joaonunoao@gmail.com>
2014-05-23 20:44:54 +0100

    New Lex and Yacc files names as defined in the new project requirements. The compiling script was updated to meet these new names.

--- compile.bash
+++ compile.bash
@@ -1,7 +1,7 @@
-flex ijparser.l &&
-bison --defines=y.tab.h ijparser.y &&
-gcc -o ijparser *.c -ll -ly -g
+flex ijcompiler.l &&
+bison --defines=y.tab.h ijcompiler.y &&
+gcc -o ijcompiler *.c -ll -ly -g
 
-rm ijparser.zip
-zip -r ijparser.zip ijparser.l ijparser.y astNodes.c astNodes.h show.c show.h symbols.c symbols.h semantic.c semantic.h exitClean.h
+rm ijcompiler.zip
+zip -r ijcompiler.zip ijcompiler.l ijcompiler.y astNodes.c astNodes.h show.c show.h symbols.c symbols.h semantic.c semantic.h exitClean.h
 
